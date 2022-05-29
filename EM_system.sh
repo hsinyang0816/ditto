94ea05c5d8b9a9845068c8d909e76019c8c53f8e
hsinyang0816 <56906673+hsinyang0816@users.noreply.github.com>
2022-05-30 03:16:00 +0800

    Update EM_system.sh

--- EM_system.sh
+++ EM_system.sh
@@ -6,7 +6,7 @@ python3 blocking/csv2txt.py --csv $1 --txt blocking/input/table_A.txt;
 python3 blocking/csv2txt.py --csv $2 --txt blocking/input/table_B.txt;
 # Blocker
 echo "-----------------------------------------------------  Start Blocking  -----------------------------------------------------"
-CUDA_VISIBLE_DEVICES=0 python3 blocking/blocker.py  --input_path blocking/input/  --left_fn table_A.txt  --right_fn table_B.txt  --output_fn ../../input/candidates.jsonl  --model_fn blocker_model  --k 5
+CUDA_VISIBLE_DEVICES=0 python3 blocking/blocker.py --input_path blocking/input/ --left_fn table_A.txt --right_fn table_B.txt --output_fn ../../input/candidates.jsonl --model_fn blocker_model --k 5;
 # Matcher
 echo "-----------------------------------------------------  Start Matching  -----------------------------------------------------"
-CUDA_VISIBLE_DEVICES=0 python3 matcher.py  --task Structured/Amazon-Google  --input_path input/candidates.jsonl  --output_path $3  --lm roberta  --max_len 128  --use_gpu --checkpoint_path checkpoints --fp16
+CUDA_VISIBLE_DEVICES=0 python3 matcher.py --task Structured/Amazon-Google --input_path input/candidates.jsonl --output_path $3 --lm roberta --max_len 128 --use_gpu --checkpoint_path checkpoints --fp16;
