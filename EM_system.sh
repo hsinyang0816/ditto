6b6f4931a7aaa80b75702fb8ae592913fdecf7f6
hsinyang0816 <56906673+hsinyang0816@users.noreply.github.com>
2022-05-30 00:17:57 +0800

    Update EM_system.sh

--- EM_system.sh
+++ EM_system.sh
@@ -2,6 +2,7 @@
 [ -f checkpoints/Structured/Amazon-Google/model.pt ] && echo "Model exists" || wget -O checkpoints/Structured/Amazon-Google/model.pt 'https://www.dropbox.com/s/79xil3arrq1vdn3/model.pt?dl=1'
 [ -f blocker_model.zip ] && echo "Model exists" || wget -O blocker_model.zip 'https://www.dropbox.com/s/k2x6b7lnwlcapy3/blocker_model.zip?dl=1'
 unzip blocker_model.zip
+rm blocker_model.zip
 # Transform csv to txt
 python3 blocking/csv2txt.py --csv $1 --txt blocking/input/table_A.txt
 python3 blocking/csv2txt.py --csv $2 --txt blocking/input/table_B.txt
