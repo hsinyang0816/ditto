54839968abf1af8e104c0d68f466a09b90d403af
hsinyang0816 <56906673+hsinyang0816@users.noreply.github.com>
2022-05-29 22:57:26 +0800

    Update EM_system.sh

--- EM_system.sh
+++ EM_system.sh
@@ -1,6 +1,6 @@
 # Download resource
 [ -f checkpoints/Structured/Amazon-Google/model.pt ] && echo "Model exists" || wget -O checkpoints/Structured/Amazon-Google/model.pt 'https://www.dropbox.com/s/79xil3arrq1vdn3/model.pt?dl=1'
-[ -f blocker_model.zip ] && echo "Model exists" || wget -O blocker_model.zip 'https://www.dropbox.com/s/79xil3arrq1vdn3/model.pt?dl=1'
+[ -f blocker_model.zip ] && echo "Model exists" || wget -O blocker_model.zip 'https://www.dropbox.com/s/k2x6b7lnwlcapy3/blocker_model.zip?dl=1'
 unzip blocker_model.zip
 
 # Transform csv to txt
