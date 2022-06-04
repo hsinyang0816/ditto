d6219f5c914ed83ada73222713fcc001499882af
hsinyang0816 <56906673+hsinyang0816@users.noreply.github.com>
2022-06-04 17:52:59 +0800

    Update EM_system.sh

--- EM_system.sh
+++ EM_system.sh
@@ -1,6 +1,7 @@
 # Download resource
-[ -f blocker_model/pytorch_model.bin ] && echo "Model exists" || wget -O blocker_model/pytorch_model.bin 'https://www.dropbox.com/s/y9bmtj0kbzzi6zo/pytorch_model.bin?dl=1'
-[ -f checkpoints/Structured/Amazon-Google/model.pt ] && echo "Model exists" || wget -O checkpoints/Structured/Amazon-Google/model.pt 'https://www.dropbox.com/s/79xil3arrq1vdn3/model.pt?dl=1'
+[ -f blocker_model.zip ] && echo "Model exists" || wget -O blocker_model/pytorch_model.bin 'https://www.dropbox.com/s/7k6mk1cc2fixn7c/blocker_model.zip?dl=1'
+[ -f checkpoints/Dirty/DBLP-ACM/model.pt ] && echo "Model exists" || wget -O checkpoints/Dirty/DBLP-ACM/model.pt 'https://www.dropbox.com/s/8jasdygnuuqcslj/model.pt?dl=1'
+unzip -f blocker_model.zip;
 # Transform csv to txt
 python3 blocking/csv2txt.py --csv $1 --txt blocking/input/table_A.txt;
 python3 blocking/csv2txt.py --csv $2 --txt blocking/input/table_B.txt;
@@ -9,4 +10,4 @@ echo "-----------------------------------------------------  Start Blocking  ---
 CUDA_VISIBLE_DEVICES=0 python3 blocking/blocker.py --input_path blocking/input/ --left_fn table_A.txt --right_fn table_B.txt --output_fn ../../input/candidates.jsonl --model_fn blocker_model --k 5;
 # Matcher
 echo "-----------------------------------------------------  Start Matching  -----------------------------------------------------"
-CUDA_VISIBLE_DEVICES=0 python3 matcher.py --task Structured/Amazon-Google --input_path input/candidates.jsonl --output_path $3 --lm roberta --max_len 128 --use_gpu --checkpoint_path checkpoints --fp16;
+CUDA_VISIBLE_DEVICES=0 python3 matcher.py --task Dirty/DBLP-ACM --input_path input/candidates.jsonl --output_path $3 --lm roberta --max_len 128 --use_gpu --checkpoint_path checkpoints --fp16;
