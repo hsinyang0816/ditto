4224cc5da5e5c36b61ca02fd74beff9e47e8e413
hsinyang0816 <56906673+hsinyang0816@users.noreply.github.com>
2022-06-06 22:52:12 +0800

    Add files via upload

---
+++ DITTO_combine.py
@@ -0,0 +1,157 @@
+'''
+    This program is used to combined all data in the two databases (left_DB/right_DB) according to DITTO result.
+    If no database is created, please execute "create_database.py" first.
+'''
+import mysql.connector
+import jsonlines
+
+# ---------- Arugment Setup ----------
+# Input jsonline file
+filePath = "output.jsonl"
+# Database Account
+hostname = "localhost"
+username = "root"
+db_root_password = "Cc5331751501"
+# Database Name
+database_ori_left = "left_DB"
+database_ori_right = "right_DB"
+database_result = "result_DB"
+# -----------------------------------
+
+def readJsonlFile(file):
+    result = []
+    try:
+        with jsonlines.open(file) as reader:
+            for line in reader:
+                matching = line['match']
+                # Check if the line is matched
+                # Not Matched => not recorded
+                if matching == 0:
+                    continue
+                # Matched => record left/right id
+                left_match_id = line['left_id']
+                right_match_id = line['fight_id']
+                result.append((left_match_id, right_match_id))
+    except EnvironmentError:
+        print("No file can be opened! Please check if your file exists in => " + str(file))
+
+    return result
+
+
+def DITTO_combine_database():
+    global filePath, db_root_password, hostname, username, db_root_password
+    global database_ori_left, database_ori_right, database_result
+    
+    # Database Connection
+    resultdb = mysql.connector.connect(
+        host=hostname, 
+        user=username, 
+        passwd=db_root_password, 
+        database=database_result)
+
+    # Check database connection
+    if not resultdb.is_connected():
+        print("Connecting to \"result\" database failed! Please check your connection!")
+        return
+    
+    print("All database is connected!\n\n")
+    cursor = resultdb.cursor(buffered=True)
+
+    matching_list = readJsonlFile(filePath)
+    
+    # sql_insert = "INSERT INTO result_DB.Citation (title, authors, venue, year)\
+    #                 SELECT\
+    #                 GROUP_CONCAT(L.title, ' ', R.title) as title,\
+    #                 GROUP_CONCAT(L.authors, ' ', R.authors) as author,\
+    #                 GROUP_CONCAT(L.venue, ' ', R.venue) as venue,\
+    #                 GROUP_CONCAT(L.year, ' ', R.year) as year\
+    #                 FROM left_DB.Citation as L, right_DB.Citation as R\
+    #                 WHERE L.id=%s AND R.id=%s;"
+    
+    sql_insert = "INSERT result_DB.Citation(title, authors, venue, year)\
+                    VALUES (%s, %s, %s, %s)"
+
+    sql_findLeft = "SELECT left_DB.Citation.title, left_DB.Citation.authors, left_DB.Citation.venue, left_DB.Citation.year FROM left_DB.Citation\
+                    WHERE left_DB.Citation.id = %s;"
+
+    sql_findRight = "SELECT right_DB.Citation.title, right_DB.Citation.authors, right_DB.Citation.venue, right_DB.Citation.year FROM right_DB.Citation\
+                    WHERE right_DB.Citation.id = %s;"
+    
+
+    sql_deleteLeft= "DELETE FROM left_DB.Citation\
+                    WHERE left_DB.Citation.id=%s;"
+    
+    sql_deleteRight= "DELETE FROM right_DB.Citation\
+                    WHERE right_DB.Citation.id=%s;"
+    
+    print("Trying to insert data that matches...")
+    modified_count = 0
+    for data in matching_list:
+        # val = tuple(data)
+        list_L = [data[0]]
+        list_R = [data[1]]
+        val_l = tuple(list_L)
+        val_r = tuple(list_R)
+        
+        cursor.execute(sql_findLeft, val_l)
+        Left_result = cursor.fetchone()
+        if Left_result is None:
+            continue
+        Left_result = [i for i in Left_result]
+        cursor.execute(sql_findRight, val_r)
+        Right_result = cursor.fetchone()
+        if Right_result is None:
+            continue
+        Right_result = [i for i in Right_result]
+
+        matched = []
+
+        for idx in range(len(Left_result)):
+            if Left_result[idx] == "":
+                matched.append(Right_result[idx])
+            else:
+                matched.append(Left_result[idx])
+        
+        matched = tuple(matched)
+        cursor.execute(sql_insert, matched)
+
+        cursor.close()
+        cursor = resultdb.cursor(buffered=True)
+
+        cursor.execute(sql_deleteLeft, val_l)
+
+        cursor.close()
+        cursor = resultdb.cursor(buffered=True)
+
+        cursor.execute(sql_deleteRight, val_r)
+
+        cursor.close()
+        cursor = resultdb.cursor(buffered=True)
+        
+        resultdb.commit()
+        modified_count += 1
+    print("All Matching Data(In total : %s) in output file has inserted into result_DB.\n" %modified_count)
+    
+    cursor.close()
+    cursor = resultdb.cursor(buffered=True)
+
+    sql_insertLeft = "INSERT INTO result_DB.Citation (title, authors, venue, year)\
+                    SELECT L.title, L.authors, L.venue, L.year\
+                    FROM left_DB.Citation as L;"
+    sql_insertRight = "INSERT INTO result_DB.Citation (title, authors, venue, year)\
+                    SELECT R.title, R.authors, R.venue, R.year\
+                    FROM right_DB.Citation as R;"
+    cursor.execute(sql_insertLeft)
+    cursor.execute(sql_insertRight)
+    cursor.execute("Truncate TABLE left_DB.Citation")
+    cursor.execute("Truncate TABLE right_DB.Citation")
+    print("\nAll Remaining Data(In left_DB/right_DB) has combined into result_DB. \n\n")
+
+    # Close all connection
+    cursor.close()
+    resultdb.close()
+
+    print("[ ===== Task Complete! Shutting down all connection! ===== ]\n\n")
+
+if __name__ == '__main__':
+    DITTO_combine_database()
---
+++ create_database.py
@@ -0,0 +1,185 @@
+import mysql.connector
+import csv
+
+'''
+    This program is used to create three necessary databases by reading csv file.
+'''
+
+# ---------- Arugment Setup ----------
+# Database Account
+''' Online '''
+hostname = "192.168.0.10"
+username = "guest"
+db_root_password = "sqlfinalproject"
+
+''' Local
+hostname = "localhost"
+username = "root"
+db_root_password = "123456"
+'''
+# Input Data File Path
+filePath_tableA = "D:/ProgrammingCode/DBMS/final_project/tableA.csv"
+filePath_tableB = "D:/ProgrammingCode/DBMS/final_project/tableB.csv"
+# -----------------------------------
+
+def createDatabase():
+    global filePath_tableA, filePath_tableB
+    global hostname, username, db_root_password
+    # ===== Database Initialization =====
+    # Create Database / naming => (left_DB, right_DB, result_DB)
+    mydb = mysql.connector.connect(
+        host=hostname, 
+        user=username, 
+        passwd=db_root_password)
+
+    cursor = mydb.cursor()
+    try:
+        print("\n\nTrying to delete existing database...\n")
+        cursor.execute("DROP DATABASE left_DB")
+        cursor.execute("DROP DATABASE right_DB")
+        cursor.execute("DROP DATABASE result_DB")
+    except mysql.connector.Error as err:
+        print("Database does not Exist!\n")
+    
+    print("Trying to create database......")
+    cursor.execute("CREATE DATABASE left_DB")
+    cursor.execute("CREATE DATABASE right_DB")
+    cursor.execute("CREATE DATABASE result_DB")
+    print("\nDatabase construction complete!")
+
+    # Finish creating database, shutting down connection
+    cursor.close()
+    mydb.close()
+
+    # ===== Inserting Data into Database separately =====
+    # ----- Insert data into left_DB -----
+    mydb = mysql.connector.connect(
+        host=hostname, 
+        user=username, 
+        passwd=db_root_password, 
+        database="left_DB")
+
+    if not mydb.is_connected():
+        print("Connecting to left_DB failed! Please check your connection!")
+        return
+    
+    # Print database version if connected to database
+    db_info = mydb.get_server_info()
+    print("\nDatabase Version:", db_info, "\n")
+
+    cursor = mydb.cursor()
+
+    # Create table "Citation"
+    # id | title | authors | venue | year
+    sql = "CREATE TABLE Citation (id INT PRIMARY KEY, title TEXT, authors VARCHAR(1024), venue VARCHAR(255), year VARCHAR(20));"
+    cursor.execute(sql)
+    print("CREATE TABLE Citation in left_DB......\n")
+    # Read csv file
+    csv_fileA = open(filePath_tableA, encoding='utf-8', newline='')
+    csv_data = csv.reader(csv_fileA, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    # Skip the first column
+    next(csv_data)
+
+    print("Trying to insert data from csv file!")
+    # Insert from csv file
+    sql = "INSERT INTO Citation (id, title, authors, venue, year) VALUES (%s, %s, %s, %s, %s)"
+    count = 0
+    for row in csv_data:
+        for i in range(len(row)):
+            if row[i] == '':
+                row[i] = None
+        val = tuple(row)
+        cursor.execute(sql, val)
+        mydb.commit()
+        count += 1
+    csv_fileA.close()
+    print("All Data(In total : %s rows) in csv file was inserted. \n" %count)
+
+    # Finish all task, shutting down connection
+    cursor.close()
+    mydb.close()
+    print("left_DB Closed!", "\n\n")
+    
+    # -------------------------------------
+    # ----- Insert data into right_DB -----
+    mydb = mysql.connector.connect(
+        host=hostname, 
+        user=username, 
+        passwd=db_root_password, 
+        database="right_DB")
+
+    if not mydb.is_connected():
+        print("Connecting to right_DB failed! Please check your connection!")
+        return
+    
+    # Print database version if connected to database
+    db_info = mydb.get_server_info()
+    print("Database Version:", db_info, "\n\n")
+
+    cursor = mydb.cursor()
+
+    # Create table "Citation"
+    # id | title | authors | venue | year
+    sql = "CREATE TABLE Citation (id INT PRIMARY KEY, title TEXT, authors VARCHAR(1024), venue VARCHAR(255), year VARCHAR(20));"
+    cursor.execute(sql)
+    print("CREATE TABLE Citation in right_DB......\n")
+
+    # Read csv file
+    csv_fileB = open(filePath_tableB, encoding='utf-8', newline='')
+    csv_data = csv.reader(csv_fileB, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    # Skip the first column
+    next(csv_data)
+
+    print("Trying to insert data from csv file!")
+    # Insert from csv file
+    sql = "INSERT INTO Citation (id, title, authors, venue, year) VALUES (%s, %s, %s, %s, %s)"
+    count = 0
+    for row in csv_data:
+        for i in range(len(row)):
+            if row[i] == '':
+                row[i] = None
+        val = tuple(row)
+        cursor.execute(sql, val)
+        mydb.commit()
+        count += 1
+    csv_fileB.close()
+    print("All Data(In total : %s rows) in csv file was inserted. \n" %count)
+
+    # Finish all task, shutting down connection
+    cursor.close()
+    mydb.close()
+    print("right_DB Closed!", "\n\n")
+
+    # --------------------------------------    
+    # ----- Create Table for result_DB -----
+    mydb = mysql.connector.connect(
+        host=hostname, 
+        user=username, 
+        passwd=db_root_password, 
+        database="result_DB")
+
+    if not mydb.is_connected():
+        print("Connecting to result_DB failed! Please check your connection!")
+        return
+    
+    # Print database version if connected to database
+    db_info = mydb.get_server_info()
+    print("Database Version:", db_info, "\n\n")
+
+    cursor = mydb.cursor()
+
+    # Create table "Citation"
+    # id | title | authors | venue | year
+    sql = "CREATE TABLE Citation (id INT PRIMARY KEY AUTO_INCREMENT, title TEXT, authors VARCHAR(2048), venue VARCHAR(255), year VARCHAR(20));"
+    cursor.execute(sql)
+    print("Creat TABLE Citation in result_DB")
+
+    cursor.close()
+    mydb.close()
+    print("result_DB Closed!", "\n\n")
+    # -------------------------------------
+
+    print("\n\nAll Database Creation Complete!!\n\n")
+
+if __name__ == '__main__':
+    createDatabase()
